cb7fffd752de5f949ae6a13cf85a1a4fe30b0a0e
arjun808 <arjunravimenon@gmail.com>
2019-05-25 11:52:21 +0000

    fix workspace path

--- vars/artifactdeploy.groovy
+++ vars/artifactdeploy.groovy
@@ -1,4 +1,4 @@
 def call(def server, def pom) {
 pomvalues = readMavenPom file: "${pom}"
-sh "scp ${WORKSPACE}/${JOB_NAME}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ubuntu@${server}:/home/ubuntu/deploy/${pomvalues.artifactId}.${pomvalues.packaging}"
+sh "scp ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ubuntu@${server}:/home/ubuntu/deploy/${pomvalues.artifactId}.${pomvalues.packaging}"
 }
--- vars/artifactdownload.groovy
+++ vars/artifactdownload.groovy
@@ -7,7 +7,7 @@ rtDownload (
           "files": [
             {
               "pattern": "generic-snapshot/${BUILD_NUMBER}/*/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging}",
-              "target": "${WORKSPACE}/${JOB_NAME}/${BUILD_NUMBER}/",
+              "target": "${WORKSPACE}/${BUILD_NUMBER}/",
               "flat": "true"
             }
          ]

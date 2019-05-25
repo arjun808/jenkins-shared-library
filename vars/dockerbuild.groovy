dbd42f745565549f19266c3acc3f81b57007fd1a
arjun808 <arjunravimenon@gmail.com>
2019-05-25 12:59:07 +0000

    add docker build changes

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,6 +1,6 @@
 def call(def pom) {
 pomvalues = readMavenPom file: "${pom}"
-imagename = ${pomvalues.artifactId.toLowerCase()}
+imagename = "${pomvalues.artifactId.toLowerCase()}"
 sh "mv ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}.${pomvalues.packaging} ."
 sh "docker build . -t arjun808/${imagename} --build-arg artifact=${pomvalues.artifactId}.${pomvalues.packaging}"
 sh "docker push arjun808/${imagename}"

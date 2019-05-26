ed3b7661e1b042418377edc49a9854e80fb04b56
arjun808 <arjunravimenon@gmail.com>
2019-05-26 09:32:21 +0000

    docker image fix

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -2,6 +2,6 @@ def call(def pom) {
 pomvalues = readMavenPom file: "${pom}"
 imagename = "${pomvalues.artifactId.toLowerCase()}"
 sh "mv ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ./${pomvalues.artifactId}.${pomvalues.packaging}"
-sh "docker build . -t arjun808/${imagename} --build-arg artifact=${pomvalues.artifactId}.${pomvalues.packaging}"
-sh "docker push arjun808/${imagename}"
+sh "docker build . -t arjun808/${imagename}:${BUILD_NUMBER} --build-arg artifact=${pomvalues.artifactId}.${pomvalues.packaging}"
+sh "docker push arjun808/${imagename}:${BUILD_NUMBER}"
 }

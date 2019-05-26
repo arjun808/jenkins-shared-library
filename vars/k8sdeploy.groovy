5384110124e324eb19ae0e8778f1188069d3976b
arjun808 <arjunravimenon@gmail.com>
2019-05-26 09:57:44 +0000

    docker pull fix

--- vars/k8sdeploy.groovy
+++ vars/k8sdeploy.groovy
@@ -1,6 +1,6 @@
 def call(def pom){
 pomvalues = readMavenPom file: "${pom}"
 k8appname = "${pomvalues.artifactId.toLowerCase()}"
-sh "kubectl set image deployment ${k8appname} ${k8appname}=${k8appname}:${BUILD_NUMBER}"
+sh "kubectl set image deployment ${k8appname} ${k8appname}=arjun808/${k8appname}:${BUILD_NUMBER}"
 sh "kubectl rollout status deployment ${k8appname}"
 }

2b603cabdaa00415a12664dd0242de1d66e80c67
arjun808 <arjunravimenon@gmail.com>
2019-05-27 18:20:07 +0530

    Update artifactdeploy.groovy

--- vars/artifactdeploy.groovy
+++ vars/artifactdeploy.groovy
@@ -1,7 +1,6 @@
 def call(def server, def pom, def user, def path, def dockeruser) {
 pomvalues = readMavenPom file: "${pom}"
 //sh "scp ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ${user}@${server}:${path}${pomvalues.artifactId}.${pomvalues.packaging}"
-sh "ssh ${user}@${server} \'sudo docker rm -f \$(sudo docker ps -aq)\'"
 sh "ssh ${user}@${server} \'sudo docker kill \$(sudo docker ps -q)\'"
 sh "ssh ${user}@${server} \'docker run -d -p 8090:8080 ${dockeruser}/${pomvalues.artifactId}:${BUILD_NUMBER}\'"
 sh "ssh ${user}@${server} \'docker run -d -p 8092:8080 ${dockeruser}/${pomvalues.artifactId}:${BUILD_NUMBER}\'"

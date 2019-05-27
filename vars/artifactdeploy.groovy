46eabfa0142b38114601bf17ae8ca6954ec59231
arjun808 <arjunravimenon@gmail.com>
2019-05-27 10:27:00 +0530

    Update artifactdeploy.groovy

--- vars/artifactdeploy.groovy
+++ vars/artifactdeploy.groovy
@@ -1,4 +1,4 @@
-def call(def server, def pom, def path) {
+def call(def server, def pom, def user, def path) {
 pomvalues = readMavenPom file: "${pom}"
-sh "scp ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ubuntu@${server}:${path}${pomvalues.artifactId}.${pomvalues.packaging}"
+sh "scp ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ${user}@${server}:${path}${pomvalues.artifactId}.${pomvalues.packaging}"
 }

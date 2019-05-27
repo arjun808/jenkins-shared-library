ef5dcbd70d5ed4d5635d48c1f00927f3e351aca6
arjun808 <arjunravimenon@gmail.com>
2019-05-27 16:13:43 +0530

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,7 @@
-def call(def pom, def duser) {
+def call(def pom, def duser, def dburl, def dbuser, def dbpassword) {
 pomvalues = readMavenPom file: "${pom}"
 imagename = "${pomvalues.artifactId.toLowerCase()}"
 sh "mv ${WORKSPACE}/${BUILD_NUMBER}/${pomvalues.artifactId}-${pomvalues.version}.${pomvalues.packaging} ./${pomvalues.artifactId}.${pomvalues.packaging}"
-  sh "docker build . -t ${duser}/${imagename}:${BUILD_NUMBER} --build-arg artifact=${pomvalues.artifactId}.${pomvalues.packaging}"
+  sh "docker build . -t ${duser}/${imagename}:${BUILD_NUMBER} --build-arg artifact=${pomvalues.artifactId}.${pomvalues.packaging} database_url=${dburl} database_user=${dbuser} database_password=${dbpassword}"
   sh "docker push ${duser}/${imagename}:${BUILD_NUMBER}"
 }

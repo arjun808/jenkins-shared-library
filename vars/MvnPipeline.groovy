ce577dbad2962b47b64b0acbb5df290c30ebff72
arjun808 <arjunravimenon@gmail.com>
2019-05-28 12:26:33 +0530

    Update MvnPipeline.groovy

--- vars/MvnPipeline.groovy
+++ vars/MvnPipeline.groovy
@@ -45,7 +45,9 @@ artifactdownload(_POM,_snapshotRepo)
 }
 
 stage('Docker Image Build'){
-withCredentials([usernamePassword(credentialsId: 'dbcredentials', passwordVariable: 'dbPassword', usernameVariable: 'dbUser')]) {
+	withCredentials([
+      [$class: 'UsernamePasswordMultiBinding', credentialsId: 'dbcredentials', usernameVariable: 'dbUser', passwordVariable: 'dbPassword'],
+  ]){
 	def _dbUser = dbUser
 	def _dbPassword = dbPassword
 	dockerbuild(_POM,_dockerUser,_dbUrl,_dbUser,_dbPassword)
